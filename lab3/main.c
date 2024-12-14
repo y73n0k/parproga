b9cfafb707ee70d6f65c2f8a491f00012258a85e
Aleksandr Konovalov <ytka.kek@yandex.ru>
2024-12-14 15:05:06 +0300

    lab3 done

--- lab3/main.c
+++ lab3/main.c
@@ -1,45 +1,38 @@
 int main(int argc, char **argv){
     const unsigned int length = 1 << 22;
+    unsigned int CHUNKS = 4;
     const unsigned int threads = strtoul(argv[1], NULL, 10);
     const unsigned int random_seed = strtoul(argv[2], NULL, 10);
-
-    unsigned int part_size = length / threads;
-
+    unsigned int part_size = length / CHUNKS;
     int *array = generate_random_array(random_seed, length);
-
     double time_start = omp_get_wtime();
 
-    #pragma omp parallel num_threads(threads) default(none) shared(array, threads, part_size, length)
-    {
-        int i = omp_get_thread_num();
+    #pragma omp parallel for num_threads(threads) default(none) shared(array, threads, part_size, length, CHUNKS)
+    for (unsigned int i = 0; i < CHUNKS; ++i) {
         int prev_size = i * part_size;
+        int current_size = (i == CHUNKS - 1) ? (length - prev_size) : part_size;
 
-        shellsort(array + prev_size, (i == threads - 1) ? (length - prev_size) : part_size);
+        shellsort(array + prev_size, current_size);
+    }
 
-        #pragma omp barrier
+    while (CHUNKS > 0) {
+        #pragma omp parallel for num_threads(threads) default(none) shared(array, threads, part_size, length, CHUNKS)
+        for (int j = 0; j < CHUNKS; j += 1) {
+            int start = j * (part_size << 1);
+            int mid = start + part_size - 1;
+            int end = (start + 2 * part_size - 1 < length) ? (start + 2 * part_size - 1) : (length - 1);
 
-        while (part_size < length) {
-            int j = i * (part_size << 1);
-            int mid = j + part_size - 1;
-            int right = (j + 2 * part_size - 1 < length) ? (j + 2 * part_size - 1) : (length - 1);
-            if (mid < right) {
-                merge(array, j, mid, right);
+            if (mid < end) {
+                merge(array, start, mid, end);
             }
-            
-            #pragma omp barrier
-            if (i == 0)
-                part_size *= 2;
-            #pragma omp barrier
         }
+        part_size <<= 1;
+        CHUNKS >>= 1;
     }
 
     double time_end = omp_get_wtime();
-
     assert(is_sorted(array, length));
-
     printf("%g\n", time_end - time_start);
-
     free(array);
-
     return 0;
 }
